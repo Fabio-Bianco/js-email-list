9942c4571272f2b7300ee8f388d6a0c7157b87ec
Fabio-Bianco <biancobfabio@gmail.com>
2025-03-13 17:55:27 +0100

    feat: created function generate-mail

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
-// Ciclo for per generare 10 mail;
 
-for (let i = 0; i < 10; i++) {
-  axios
-    .get("https://flynn.boolean.careers/exercises/api/random/mail") // Chiamata all'API per prendere indirizzo mail;
-    .then((response) => {
-      console.log("Email ricevuta:", response.data.response);
-    })
-    .catch((error) => {
-      console.error("Errore nella richiesta API:", error);
-    });
+function generateMail() {
+    // Ciclo for per generare 10 mail;
+    for (let i = 0; i < 10; i++) {
+        axios
+            .get("https://flynn.boolean.careers/exercises/api/random/mail") // Chiamata all'API per prendere indirizzo mail;
+            .then((response) => {
+                console.log("Email ricevuta:", response.data.response);
+            })
+            .catch((error) => {
+                console.error("Errore nella richiesta API:", error);
+            });
+    }
 }
+
+generateMail();
+
+
+
