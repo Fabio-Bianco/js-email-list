fe3bb0e96df0abcbff9cb54bf2545bee606932c7
Fabio-Bianco <biancobfabio@gmail.com>
2025-03-13 18:33:18 +0100

    feat: created 'li' Element

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 
+const ulElement = document.getElementById("email-list"); // Seleziona la lista <ul>
 // creao una funzione e la salvo in una variabile
 const generateTenMail = function() {
     // Ciclo for per generare 10 mail;
@@ -7,6 +8,12 @@ const generateTenMail = function() {
             .get("https://flynn.boolean.careers/exercises/api/random/mail") // Chiamata all'API per prendere indirizzo mail;
             .then((response) => {
                 console.log("Email ricevuta:", response.data.response);
+
+                const li = document.createElement("li");
+                li.textContent = response.data.response; // Imposta il testo dell' <li> come l'email ricevuta
+
+                // Aggiungi il <li> alla lista <ul>
+                ulElement.appendChild(li);
             })
             .catch((error) => {
                 console.error("Errore nella richiesta API:", error);
