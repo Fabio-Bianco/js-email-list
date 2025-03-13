969784e2ddd5ffbac1d8868991bbb50ee3a80c4d
Fabio-Bianco <biancobfabio@gmail.com>
2025-03-13 18:38:12 +0100

    format document

--- main.js
+++ main.js
@@ -1,28 +1,23 @@
-
 const ulElement = document.getElementById("email-list"); // Seleziona la lista <ul>
-// creao una funzione e la salvo in una variabile
-const generateTenMail = function() {
-    // Ciclo for per generare 10 mail;
-    for (let i = 0; i < 10; i++) {
-        axios
-            .get("https://flynn.boolean.careers/exercises/api/random/mail") // Chiamata all'API per prendere indirizzo mail;
-            .then((response) => {
-                console.log("Email ricevuta:", response.data.response);
 
-                const li = document.createElement("li");
-                li.textContent = response.data.response; // Imposta il testo dell' <li> come l'email ricevuta
+const generateTenMail = function () { // creao una funzione e la salvo in una variabile
+  
+  for (let i = 0; i < 10; i++) {  // Ciclo for per generare 10 mail;
+    
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/random/mail") // Chiamata all'API per prendere indirizzo mail;
+      .then((response) => {
+        console.log("Email ricevuta:", response.data.response);
 
-                // Aggiungi il <li> alla lista <ul>
-                ulElement.appendChild(li);
-            })
-            .catch((error) => {
-                console.error("Errore nella richiesta API:", error);
-            });
-    }
+        const li = document.createElement("li"); // creo l'elemento li e lo salvo in una variabie;
+        li.textContent = response.data.response; // Imposta il testo dell' <li> come l'email ricevuta;
+
+        ulElement.appendChild(li); // Aggiungi il <li> alla lista <ul>
+      })
+      .catch((error) => {
+        console.error("Errore nella richiesta API:", error);
+      });
+  }
 };
 
 generateTenMail(); //invoco la funzione
-
-
-
-
