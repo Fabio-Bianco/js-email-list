40380e885c572135545209acabcc2f18e00bbeab
Fabio-Bianco <biancobfabio@gmail.com>
2025-03-14 11:48:36 +0100

    docs: .readme

---
+++ README.md
@@ -0,0 +1,10 @@
+# Lista Email JS
+
+Questo repository contiene un progetto JavaScript per la gestione di una lista di email. Il progetto include funzionalità per aggiungere, rimuovere e visualizzare indirizzi email.
+
+## Funzionalità
+
+- Aggiungi indirizzi email alla lista
+- Rimuovi indirizzi email dalla lista
+- Visualizza tutti gli indirizzi email nella lista
+
--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 const ulElement = document.querySelector(".email-list"); // Seleziona la lista <ul>
 const refreshMail = document.getElementById("generate-button");
+const emailNumber = 10;
 
 const generateTenMail = function () { // creao una funzione e la salvo in una variabile
 
     ulElement.innerHTML = ""; // Svuota la lista prima di generare nuove email (prima del ciclo for)
   
-  for (let i = 0; i < 10; i++) {  // Ciclo for per generare 10 mail;
+  for (let i = 0; i < emailNumber; i++) {  // Ciclo for per generare 10 mail;
     
     axios
       .get("https://flynn.boolean.careers/exercises/api/random/mail") // Chiamata all'API per prendere indirizzo mail;
       .then((response) => {
         console.log("Email ricevuta:", response.data.response);
+
 
         const li = document.createElement("li"); // creo l'elemento li e lo salvo in una variabie;
         li.textContent = response.data.response; // Imposta il testo dell' <li> come l'email ricevuta;
@@ -24,5 +26,4 @@ const generateTenMail = function () { // creao una funzione e la salvo in una va
 };
 refreshMail.addEventListener("click", generateTenMail); // Associa la funzione al click del bottone
 generateTenMail(); //invoco la funzione
-
 
